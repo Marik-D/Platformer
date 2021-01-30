fdc77e9fc6aa700928f36669d87f1d3b83a81e4a
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-01-30 16:38:22 +0200

    Add basic camera controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Rigidbody2D target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = target.position;
+        transform.position = new Vector3(position.x, position.y, transform.position.z);
+    }
+}
