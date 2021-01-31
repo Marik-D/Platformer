93dc1ec382cbe6e32b852820b3b08c6f6f972a06
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-01-31 14:59:14 +0200

    Add disappearing platforms

---
+++ Assets/Scripts/DisappearingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    public float disappearTime = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            yield return new WaitForSeconds(disappearTime);
+            Destroy(gameObject);
+        }
+    }
+}
