38e5edab48a6ee432ed9b08b79ecf7bed9da4377
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-02-07 15:47:29 +0200

    Add knockback

---
+++ Assets/Scripts/PatrolingEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolingEnemy : MonoBehaviour
+{
+    public Collider2D collisionCheck;
+    
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody2DVelocity = _rigidbody2D.velocity;
+        rigidbody2DVelocity.x = -4f * transform.localScale.x;
+        _rigidbody2D.velocity = rigidbody2DVelocity;
+
+        if (collisionCheck.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            var transformLocalScale = transform.localScale;
+            transformLocalScale.x *= -1f;
+            transform.localScale = transformLocalScale;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.rigidbody.AddForce(((other.transform.position - transform.position).normalized + Vector3.up) * 20f, ForceMode2D.Impulse);
+        }
+    }
+}
